0e091adba014e9b14112aa1b40a734f402243d48
Pavlozavr <pavlik.cosmonaut@gmail.com>
2019-12-29 21:10:07 +0000

    Comment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+console.log('Hello world');
+console.log('Hello' + ' ' + 'world');
+document.getElementById("mama").innerHTML = 'Hello Misha!' ;
+
+let a;
+a = document.getElementById('mama').innerHTML;
+a = ("Hello Misha");
+
+let b;
+b = 
+
+alert ("Are you Michael???");
